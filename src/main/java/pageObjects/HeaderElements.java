721988ca670e92031913c54a2855caee2f2f576b
AniaPasyk <aniapasyk@gmail.com>
2024-05-23 12:35:30 +0200

    Changing architecture of test:
    WelcomePageElements interface has been replaced by HeaderPageEvents which contains elements include in the header section.

---
+++ src/main/java/pageObjects/HeaderElements.java
@@ -0,0 +1,10 @@
+package pageObjects;
+
+public interface HeaderElements {
+
+    String welcomeToMedscapeImg = "//img[@class='imageLogo']";
+    String generalMenu = "//span[text()=' General ']";
+    String generalList = "//div[@id='mat-menu-panel-1']";
+    String devices = "//div[text()='Devices']";
+    String contentTypes = "//div[text()='Content Types']";
+}
